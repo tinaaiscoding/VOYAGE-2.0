4e78f7e75e98cbfd821a02eb150b02b71de031a1
tinanvo <tinanhivo@gmail.com>
2023-09-25 20:57:05 +1000

    Removed console log in supabase config file

--- voyage-2/src/config/supabase.js
+++ voyage-2/src/config/supabase.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://npmxhddjylqvanqymibk.supabase.co';
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-console.log(process.env)
-
 export const supabase = createClient(supabaseUrl, supabaseKey)
